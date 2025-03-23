7dfa0f91829fd5892a8d831ce2f2f11cb51a500f
fintech-vit <gokulakrishnanmuthuswamyvit@gmail.com>
2025-03-23 17:16:38 +0530

    updated mote

--- main.py
+++ main.py
@@ -1,22 +1,4 @@
-# from transformers import AutoModel, AutoProcessor
 
-# model_name = "gokaygokay/Florence-2-Flux-Large"
-# model = AutoModel.from_pretrained(model_name, trust_remote_code=True)
-# processor = AutoProcessor.from_pretrained(model_name, trust_remote_code=True)
-
-# from PIL import Image
-# import torch
-
-# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-# model.to(device).eval()
-
-# image = Image.open("/home/gokul_vit/Desktop/extra/hua.png").convert("RGB")
-# inputs = processor(text="<DESCRIPTION> Describe this image in great detail.", images=image, return_tensors="pt").to(device)
-# generated_ids = model.generate(input_ids=inputs["input_ids"], pixel_values=inputs["pixel_values"], max_new_tokens=1024, num_beams=3, repetition_penalty=1.10)
-# generated_text = processor.batch_decode(generated_ids, skip_special_tokens=False)[0]
-# parsed_answer = processor.post_process_generation(generated_text, task="<DESCRIPTION>", image_size=(image.width, image.height))
-
-# print(parsed_answer["<DESCRIPTION>"])
 
 
 from transformers import AutoModelForCausalLM, AutoProcessor, AutoConfig
@@ -53,7 +35,7 @@ def run_ocr(image):
     return parsed_answer["<OCR>"]
 
 # Load image
-image_path = "/home/gokul_vit/Desktop/extra/imag2.png"
+image_path = "/home/gokul_vit/Desktop/extra/hua.png"
 image = Image.open(image_path)
 
 # Perform OCR
